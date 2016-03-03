1149feed535b4c33d80e262acb0028afa7a5c0de
zacanger <zac@zacanger.com>
2016-03-03 12:01:56 -0700

    :heavy_plus_sign: rewriting the start.sh to be more not bad

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir db ; mongod --dbpath=db/ --fork --nojournal --syslog & node server & node index &
+# creates db directory in project root
+mkdir db
+
+# runs mongod using local dir, sends to the background
+# (no journal to keep local db dir small)
+mongod --dbpath=db/ --fork --nojournal --syslog &
+
+# starts the server, sends to background
+nohup node server &
+
+# starts nw.js
+nohup node index &
 
